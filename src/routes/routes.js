521a647e88cc99341edd4321078190ff1c6e5bf4
Luis Gimenez <luisconsultor@outlook.com>
2023-02-20 08:05:58 -0300

    primer commit

---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+//* este es el archivo por donde pasan las rutas
+const express = require('express')
+
+const router = express.Router();
+
+const users = require('./users');
+const climas = require('./climas')
+
+router.use("/",users);
+router.use("/",climas)
+
+module.exports= router;
